2aaf9a5df40f3c38692b7a2613820cb363faa6b0
Moisés Ñañez <moisesnandres@hey.com>
2022-02-02 18:29:32 -0500

    Show live posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
 
   # GET /posts
   def index
-    @posts = Post.all
+    @posts = Post.live
 
     render json: @posts
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ApplicationRecord
     self.expires_at = created_at + 1.minute
   end
 
+  scope :live, ->  { where('expires_at > ?', DateTime.now.utc) }
+
   def add_30_seconds
     self.expires_at = expires_at + 30.seconds
     save!
