a1ee2d78c50bdc2da5143909bf668bfd24d557c8
Moisés Ñañez <moisesnandres@hey.com>
2022-02-04 06:54:06 -0500

    Order posts by expires_at

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
     self.expires_at = created_at + 1.minute
   end
 
-  scope :live, ->  { where('expires_at > ?', DateTime.now.utc) }
+  scope :live, ->  { where('expires_at > ?', DateTime.now.utc).order(expires_at: :asc) }
 
   def add_30_seconds
     self.expires_at = expires_at + 30.seconds
